c8e01f200dd60b166627a4e287af94442f23bd08
divyadhimaan <dhimandivya713@gmail.com>
2025-04-06 19:02:09 +0530

    movie scrolls added to main page

--- src/components/scroll.tsx
+++ src/components/scroll.tsx
@@ -3,11 +3,13 @@ import { movies } from "@/resources/movies";
 
 export default function Scroll() {
   const recommended = movies.slice(0, 8);
-  const hindi = movies.slice(8, 15);
+  const hindi = movies.slice(13, 19);
+  const watchAgain = movies.slice(8,15);
 
   return (
     <main className="bg-[#0f172a] min-h-screen px-6 py-8">
       <MovieScroll title="Recommended movies" movies={recommended} />
+      <MovieScroll title="Watch Again" movies={watchAgain} />
       <MovieScroll title="Movies originally in Hindi for you" movies={hindi} />
     </main>
   );
